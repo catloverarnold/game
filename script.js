1435564d43e6682e64df94d83e6a8dbd8b7cc72e
catloverarnold <149314262+catloverarnold@users.noreply.github.com>
2023-10-29 15:02:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+const gameArea = document.getElementById("gameArea");
+const scoreElement = document.getElementById("score");
+let score = 0;
 
-function checkGuess() {
-    const guess = document.getElementById("guessInput").value;
-    const feedback = document.getElementById("feedback");
+function createBalloon() {
+    const balloon = document.createElement("div");
+    balloon.classList.add("balloon");
+    balloon.style.left = Math.random() * (gameArea.clientWidth - 50) + "px"; // Random horizontal position
+    gameArea.appendChild(balloon);
 
-    if (guess == randomNumber) {
-        feedback.textContent = "Congratulations! You guessed it right!";
-        feedback.style.color = "green";
-    } else if (guess < randomNumber) {
-        feedback.textContent = "Too low! Try again.";
-        feedback.style.color = "red";
-    } else {
-        feedback.textContent = "Too high! Try again.";
-        feedback.style.color = "red";
-    }
+    balloon.addEventListener("click", popBalloon);
+
+    // Animation to make the balloon float upwards
+    let position = 0;
+    const interval = setInterval(() => {
+        if (position > gameArea.clientHeight) {
+            clearInterval(interval);
+            gameArea.removeChild(balloon);
+        } else {
+            position++;
+            balloon.style.bottom = position + "px";
+        }
+    }, 10);
 }
+
+function popBalloon(event) {
+    score++;
+    scoreElement.textContent = score;
+    event.target.removeEventListener("click", popBalloon);
+    gameArea.removeChild(event.target);
+}
+
+// Create balloons every 2 seconds
+setInterval(createBalloon, 2000);
