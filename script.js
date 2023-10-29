f2121703fce2707cfe9a8a447774ef8ce9fd01b9
catloverarnold <149314262+catloverarnold@users.noreply.github.com>
2023-10-29 18:17:47 +0000

    Update script.js for cat post idea

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-const gameArea = document.getElementById("gameArea");
-const scoreElement = document.getElementById("score");
-let score = 0;
+// Add this code to handle the "Next" button for Instagram posts
+document.addEventListener('DOMContentLoaded', () => {
+  // ... existing game code ...
 
-function createBalloon() {
-    const balloon = document.createElement("div");
-    balloon.classList.add("balloon");
-    balloon.style.left = Math.random() * (gameArea.clientWidth - 50) + "px"; // Random horizontal position
-    gameArea.appendChild(balloon);
+  const posts = document.querySelectorAll('#instagram-container blockquote');
+  let currentPost = 0;
 
-    balloon.addEventListener("click", popBalloon);
-
-    // Animation to make the balloon float upwards
-    let position = 0;
-    const interval = setInterval(() => {
-        if (position > gameArea.clientHeight) {
-            clearInterval(interval);
-            gameArea.removeChild(balloon);
-        } else {
-            position++;
-            balloon.style.bottom = position + "px";
-        }
-    }, 10);
-}
-
-function popBalloon(event) {
-    score++;
-    scoreElement.textContent = score;
-    event.target.removeEventListener("click", popBalloon);
-    gameArea.removeChild(event.target);
-}
-
-// Create balloons every 2 seconds
-setInterval(createBalloon, 2000);
+  document.getElementById('next-post').addEventListener('click', () => {
+    posts[currentPost].style.display = 'none';
+    currentPost = (currentPost + 1) % posts.length;
+    posts[currentPost].style.display = 'block';
+  });
+});
